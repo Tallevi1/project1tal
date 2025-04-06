301965565bb4a6f6fe4396132bbae2d7625fb64e
Tallevi1 <80206002+Tallevi1@users.noreply.github.com>
2025-04-06 13:55:10 +0300

    Add files via upload

---
+++ plot1 (1).R
@@ -0,0 +1,21 @@
+#set directory
+setwd("C:/Users/Ariella's laptop/OneDrive - weizmann.ac.il/MSc weizmann/Coursera exploratory data analysis/final assignment")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Aggregate the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008
+AT <- aggregate(Emissions ~ year,NEI, sum)
+
+# Create the PNG file with specified width and height
+png("plot1.png", width=480, height=480)
+
+#plot data for plot1
+barplot((AT$Emissions)/10^6, 
+        names.arg = AT$year,
+        xlab = "Year",
+        ylab = "PM2.5 Emissions (10^6 Tons)",
+        main = "Total PM2.5 Emissions From All US Sources",
+        col = "black")
+
+# close the PNG file
+dev.off()
